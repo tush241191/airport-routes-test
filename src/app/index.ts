6ea32e456514fc8f1eb0811da866d5fec91f6509
Tushar Kharat <tushar.kharat@kwota.com>
2023-07-17 11:41:23 +0300

    Removing duplicate routes

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,12 +2,23 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 
 import { notNil, flatten } from '../util';
-import { Airport, loadAirportData } from '../data';
+import { Airport, loadAirportData, loadRouteData } from '../data';
 
 export async function createApp() {
   const app = express();
 
   const airports = await loadAirportData();
+  const routesDb = await loadRouteData();
+
+  // Removing duplicate routes
+  const routes = routesDb.reduce((accumulator, current) => {
+    const isDuplicate = accumulator.some(item => item.source.id === current.source.id && item.destination.id === current.destination.id);
+    if (!isDuplicate) {
+      accumulator.push(current);
+    }
+    return accumulator;
+  }, []);
+
   const airportsByCode = new Map<string, Airport>(
     flatten(airports.map((airport) => [
       airport.iata !== null ? [airport.iata.toLowerCase(), airport] as const : null,
@@ -31,6 +42,25 @@ export async function createApp() {
 
     return res.status(200).send(airport);
   });
+
+  // API endpoint to find all routes
+  app.get('/routes', (req, res) => {
+    return res.status(200).send(routes);
+  });
+
+  // API endpoint to find routes from source airport
+  app.get('/routes/:source', (req, res) => {
+    const source = req.params['source'];
+    
+    if (source === undefined) {
+      return res.status(400).send('Must provide source airport');
+    }
+  
+    const sourceAirport = airportsByCode.get(source.toLowerCase());
+
+    const sourceRoutes = routes.filter((route) => route.source.id === sourceAirport.id);
+    return res.status(200).send(sourceRoutes);
+  })
 
   app.get('/routes/:source/:destination', (req, res) => {
     const source = req.params['source'];
